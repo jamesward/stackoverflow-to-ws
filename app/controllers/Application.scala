de011463fcc6bd24bdaba6fa82a90773fb9886ab
James Ward <james@jamesward.com>
2019-08-30 14:03:24 -0600

    add error handling

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,15 +3,17 @@ package controllers
 import akka.stream.scaladsl.{Flow, Sink, Source}
 import com.google.cloud.bigquery.{FieldValueList, LegacySQLTypeName, Schema}
 import javax.inject.{Inject, Singleton}
+import play.api.Logging
 import play.api.libs.json._
 import play.api.mvc.{InjectedController, WebSocket}
 
+import scala.concurrent.Future
 import scala.concurrent.duration._
 import scala.jdk.CollectionConverters._
 import scala.util.Try
 
 @Singleton
-class Application @Inject() extends InjectedController {
+class Application @Inject() extends InjectedController with Logging {
 
   def index = Action { implicit request =>
     Ok(views.html.index(routes.Application.questions().webSocketURL()))
@@ -35,17 +37,21 @@ class Application @Inject() extends InjectedController {
     }
   }
 
-  def questions = WebSocket.accept[JsValue, JsValue] { _ =>
+  def questions = WebSocket.acceptOrResult[JsValue, JsValue] { _ =>
     val query = """
                   |SELECT CONCAT('https://stackoverflow.com/questions/', CAST(id as STRING)) as url, title, view_count, favorite_count
                   |FROM `bigquery-public-data.stackoverflow.posts_questions`
                   |ORDER BY favorite_count DESC
                   |""".stripMargin
 
-    val (schema, questions) = StackOverflowBigQuery.query(query)
-    val questionSource = Source.fromIterator(() => questions.iterator)
-    val source = Source.tick(Duration.Zero, 1.second, schema).zip(questionSource).map(Json.toJson(_))
-    Flow.fromSinkAndSource(Sink.ignore, source)
+    StackOverflowBigQuery.query(query).fold({ t =>
+      logger.error("Query error", t)
+      Future.successful(Left(InternalServerError(t.getMessage)))
+    }, { case (schema, questions) =>
+      val questionSource = Source.fromIterator(() => questions.iterator)
+      val source = Source.tick(Duration.Zero, 1.second, schema).zip(questionSource).map(Json.toJson(_))
+      Future.successful(Right(Flow.fromSinkAndSource(Sink.ignore, source)))
+    })
   }
 
 }
@@ -55,14 +61,16 @@ object StackOverflowBigQuery {
 
   import com.google.cloud.bigquery.{BigQuery, BigQueryOptions, FieldValueList, JobId, JobInfo, QueryJobConfiguration}
 
-  def query(query: String): (Schema, Iterable[FieldValueList]) = {
-    val bigQuery = BigQueryOptions.getDefaultInstance.getService
-    val jobId = JobId.of(UUID.randomUUID().toString)
-    val queryConfig = QueryJobConfiguration.newBuilder(query).setUseLegacySql(false).build()
-    val queryJob = bigQuery.create(JobInfo.newBuilder(queryConfig).setJobId(jobId).build())
-    queryJob.waitFor()
-    val page = queryJob.getQueryResults(BigQuery.QueryResultsOption.pageSize(1))
-    page.getSchema -> page.iterateAll().asScala
+  def query(query: String): Try[(Schema, Iterable[FieldValueList])] = {
+    Try {
+      val bigQuery = BigQueryOptions.getDefaultInstance.getService
+      val jobId = JobId.of(UUID.randomUUID().toString)
+      val queryConfig = QueryJobConfiguration.newBuilder(query).setUseLegacySql(false).build()
+      val queryJob = bigQuery.create(JobInfo.newBuilder(queryConfig).setJobId(jobId).build())
+      queryJob.waitFor()
+      val page = queryJob.getQueryResults(BigQuery.QueryResultsOption.pageSize(1))
+      page.getSchema -> page.iterateAll().asScala
+    }
   }
 
 }
